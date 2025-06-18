479f159a2eda1f33e4896359f5b714efe8090d6f
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-18 12:13:25 +0300

    m20

---
+++ task20.py
@@ -0,0 +1,12 @@
+savol = "O'zbekiston poytaxti nima "
+
+while True:
+    print(savol)
+    javob = input("Javobingiz>>")
+
+    if javob == savol:
+        print("Xato")
+    
+    if javob == "Toshkent":
+        print("To'gri")
+        break
