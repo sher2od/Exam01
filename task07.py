9215578194113462420f75c3be2291a2f1a06f68
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-18 11:14:17 +0300

    m7

--- task07.py
+++ task07.py
@@ -1,3 +1,7 @@
 text = "Programming"
 
-print(text[0:5])
+print(text[0:5])  # start, stop, step
+
+# start = 0 >> boshlash
+# stop = 5 >> toxtatish
+# step = deffolt holatda 1 ga teng >> qadamlar soni
