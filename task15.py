1db2cd907aa01c7ece073db91f387f470dbb733e
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-18 11:44:22 +0300

    m15

---
+++ task15.py
@@ -0,0 +1,10 @@
+number = 30
+
+if number % 2 == 0:
+    print(f"{number} soni 2 ga bolinadi")
+if number % 3 == 0:
+    print(f"{number} soni 3 ga bolinadi")
+if number % 5 == 0:
+    print(f"{number} soni 5 ga bolinadi")
+else:
+    print(f"Bu sonlar {number} ga bolinmaydi")
