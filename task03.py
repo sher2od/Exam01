232a77e2eb2c1c2aa5dd91e16f22e76e1fd97d8d
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-18 11:02:19 +0300

    m3

---
+++ task03.py
@@ -0,0 +1,10 @@
+num1 = float(input("1 - son >> "))
+num2 = float(input("2 - son >> "))
+num3 = float(input("3 - son >> "))
+
+# sonning orta arifmetigini topish uchun berilgan 
+# barcha sonlarni qoshib sonlar soniga bolamiz
+
+result = (num1 + num2 + num3) / 3
+
+print("Natijasi >> ",result )
